b66b100bb15e76a2f4a6f9d662236a64a530392a
juansito606 <juanesizquierdo71@gmail.com>
2026-02-17 14:46:41 +0000

    tarea

---
+++ Ejercicios/ejercicio3.java
@@ -0,0 +1,13 @@
+public class ejercicio3 {
+    public static void main(String[] args) {
+        
+        double tarifa = 10.0;
+        double horas_dia = 8.0;
+
+        //calculo
+
+        double salario_semanal= tarifa * horas_dia * 7;
+
+        System.out.println(salario_semanal);
+    }
+}
---
+++ Ejercicios/ejercicios4.java
@@ -0,0 +1,16 @@
+public class ejercicios4 {
+    public static void main(String[] args) {
+        
+        //datos
+
+        double a = 2.0;
+        double b = 5;
+
+        //resultado
+
+        double resultado = 3 * a - 4 * b/ Math.pow(a,2);
+
+        System.out.println(resultado);
+    }
+    
+}
