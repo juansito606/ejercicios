20772b641c6d4eb6237c68336f9a03f8231325f9
juansito606 <juanesizquierdo71@gmail.com>
2026-02-17 13:05:53 +0000

    tarea

---
+++ Ejercicios/Ejercicios.java
@@ -0,0 +1,24 @@
+public class Ejercicios {
+    public static void main (String [] args) {
+
+        //Datos triangulo
+        double base = 10.0;
+        double altura = 5.0;
+
+        // calculo
+
+        double superficie = base*altura/2;
+
+        //Resultado
+
+        System.out.println(superficie);
+    
+
+    }
+}
+
+
+
+
+
+
---
+++ Ejercicios/ejercicio2.java
@@ -0,0 +1,10 @@
+public class ejercicio2 {
+    public static void main(String[] args) {
+        double r = 5.0;
+        double L = 2 * Math.PI * r;
+        double A = Math.PI * r *r;
+
+        System.out.println(L);
+        System.out.println(A);
+    }
+}
