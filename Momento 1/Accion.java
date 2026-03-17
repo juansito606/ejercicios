306802c7cf741e298548debc036ef8587ec3a601
juansito606 <juanesizquierdo71@gmail.com>
2026-03-17 05:22:38 +0000

    modificacion

--- Momento 1/Accion.java
+++ Momento 1/Accion.java
@@ -1,12 +1,52 @@
+import java.util.Scanner;
+
 public class Accion {
     public static void main(String[] args) {
-        CuentaBancaria cuenta1 = new CuentaBancaria();
+        Scanner sc = new Scanner (System.in);
+        CuentaBancaria cuenta = new CuentaBancaria();
 
-        cuenta1.numeroCuenta="001";
-        cuenta1.titular="Juan";
+        //Interaccion
+        System.out.println("Ingrese nombre del titular:  ");
+        cuenta.titular = sc.nextLine();
+        System.out.println("Ingrese numero de cuenta: ");
+        cuenta.numeroCuenta = sc.nextLine();
 
-        cuenta1.depositar(500);
-        cuenta1.retirar(200);
-        cuenta1.consultarSaldo();
+        int opcion;
+        do {
+            System.out.println("MENÚ BANCARIO");
+            System.out.println("1. Consultar Saldo");
+            System.out.println("2. Depositar Dinero");
+            System.out.println("3. Retirar Dinero");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    cuenta.consultarSaldo();
+                    break;
+                case 2:
+                    System.out.print("Cantidad a depositar: ");
+                    double dep = sc.nextDouble();
+                    cuenta.depositar(dep);
+                    break;
+                case 3:
+                    System.out.print("Cantidad a retirar: ");
+                    double ret = sc.nextDouble();
+                    cuenta.retirar(ret);
+                    break;
+                case 4:
+                    System.out.println("Gracias por usar nuestro banco. ¡Adiós!");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 4);
+
+        sc.close();
     }
 }
+
+
+
+      
--- Momento 1/CuentaBancaria.java
+++ Momento 1/CuentaBancaria.java
@@ -7,22 +7,28 @@ public class CuentaBancaria {
 
     //Metodo para depositar
     public void depositar (double cantidad){
-        saldo = saldo + cantidad;
-        System.out.println("El titular " + titular + " deposito " + cantidad);
+        if (cantidad > 0){
+            saldo += cantidad;
+            System.out.println("Deposito aprobado. Nuevo saldo: $" + saldo);
+        }
     }
 
     //metodo para retirar
     public void retirar (double cantidad){
-        if(cantidad <= saldo){
-            saldo= saldo - cantidad;
-            System.out.println("El titular " + titular + " retiro: " + cantidad);
+        if(cantidad >0 && cantidad <= saldo){
+            saldo -= cantidad;
+            System.out.println("Retiro exitoso. Saldo Restante: $" + saldo);
         }else{
             System.out.println("No hay dinero suficiente para retirar");
         }
     }         
           //metodo para consultar saldo
     public void consultarSaldo(){
-        System.out.println("El saldo actual de " + titular + " es: " + saldo);
+        System.out.println("Estado de cuenta");
+        System.out.println("Titular: " + titular);
+        System.out.println("Cuenta: " + numeroCuenta);
+        System.out.println("Saldo Actual: $" + saldo);
+        System.out.println("-------------");
     }
  }
 
